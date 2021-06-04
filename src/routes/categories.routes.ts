2148ce9b4982f499b3c7a8b40a39df4ae4f6a0f0
Carlos A <carlosmiranda19122@gmail.com>
2021-06-04 16:14:25 -0300

    created Category model

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,27 @@
 import { Router } from "express";
 import { v4 as uuidv4 } from "uuid";
 
+import { Category } from "../model/Category";
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidv4(),
+  const category = new Category();
+
+  // 'Object' is responsible for taking an object and assigning data
+  Object.assign(category, {
     name,
     description,
-  };
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json(category);
 });
 
 export { categoriesRoutes };
