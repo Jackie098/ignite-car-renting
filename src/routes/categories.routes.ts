18566944032504e44628f10ad017fa89cd5e57a3
Carlos A <carlosmiranda19122@gmail.com>
2021-06-09 22:24:58 -0300

    created Category repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,27 +1,16 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  // 'Object' is responsible for taking an object and assigning data
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return response.status(201).json(category);
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
