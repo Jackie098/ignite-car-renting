71a27b1641be1474cdc808e20cf509b7e341f29c
Carlos A <carlosmiranda19122@gmail.com>
2021-06-15 16:15:24 -0300

    created the model, repository and service of 'specification'

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,27 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private specificationsRepository: ISpecificationsRepository) { }
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateSpecificationService };
