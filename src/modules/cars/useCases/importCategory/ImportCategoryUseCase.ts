450e1c6dd4a955462139187ef945d219379984c4
Carlos A <carlosmiranda19122@gmail.com>
2021-06-25 20:57:30 -0300

    deleting temporary files

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -35,6 +35,7 @@ class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
